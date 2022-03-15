b05b96466d81f225d4d27f6c5471ac23a99c9794
Ikenna John Ezike <ezike_iyke@yahoo.com>
2022-03-15 13:49:10 -0400

    Added Spinner Animation

---
+++ spinner1.js
@@ -0,0 +1,36 @@
+
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+}, 100);
+
+setTimeout(() => {
+  process.stdout.write('\r/   ');
+}, 300);
+
+setTimeout(() => {
+  process.stdout.write('\r-   ');
+}, 500);
+
+setTimeout(() => {
+  process.stdout.write('\r| '); 
+}, 700);
+
+
+setTimeout(() => {
+  process.stdout.write('\r\-   '); 
+}, 1000);
+
+setTimeout(() => {
+  process.stdout.write('\r| '); 
+}, 1400);
+
+setTimeout(() => {
+  process.stdout.write('\n'); 
+}, 1500)
+
+
+
+
+
+
+process.stdout.write('hello from spinner1.js... \rheyyy come\n');
